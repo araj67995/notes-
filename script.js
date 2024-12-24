c536e31106257bf7469c5f7adca9a8cc5d559ee5
araj67995 <134071631+araj67995@users.noreply.github.com>
2024-12-24 20:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+function showModal() {
+  document.getElementById("passwordModal").style.display = "flex";
+}
+
+function validatePassword() {
+  const password = document.getElementById("passwordInput").value;
+  const correctPassword = "alpha7061"; // Set the correct password here
+  if (password === correctPassword) {
+    window.location.href = "fundamental.html"; // Redirect to the desired page
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+}
+
+
+function second() {
+    document.getElementById("passwordModala").style.display = "flex";
+  }
+  
+  function validateme() {
+    const password = document.getElementById("second").value;
+    const correctpass = "xyz7261"; // Set the correct password here
+    if (password === correctpass) {
+      window.location.href = "OS.HTML"; // Redirect to the desired page
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  }
+
+  
+
+function third() {
+    document.getElementById("passwordModalb").style.display = "flex";
+  }
+  
+  function validatemee() {
+    const password = document.getElementById("third").value;
+    const correctpass = "hr2404s"; // Set the correct password here
+    if (password === correctpass) {
+      window.location.href = "DOS.HTML"; // Redirect to the desired page
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  }
+
+
+  
+  function fourth() {
+    document.getElementById("passwordModalc").style.display = "flex";
+  }
+  
+  function validatemeee() {
+    const password = document.getElementById("fourth").value;
+    const correctpass = "9jfgJ508"; // Set the correct password here
+    if (password === correctpass) {
+      window.location.href = "ALGO.HTML"; // Redirect to the desired page
+    } else {
+      alert("Incorrect password. Please try again.");
+    }
+  }
+  
+  
